f0b9837c4714786e6887c0281d8789a3495ecb08
sarveshdakhore <sarveshd444@gmail.com>
2023-12-28 04:49:40 +0530

    t

--- src/js/timetable.js
+++ src/js/timetable.js
@@ -1717,6 +1717,7 @@ document
                     spanMsgColor = 'green';
                     // document.getElementById('slot-input').value = '';
                     document.getElementById('teacher-input_remove').value = '';
+                    document.getElementById('venue-input').value = '';
                     var teacherData = {
                         courseName: courseName,
                         slots: slotsInput,
