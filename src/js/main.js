4061eb8e5dabeb8b268c01921faefeacd0f9efd1
sarveshdakhore <sarveshd444@gmail.com>
2024-01-02 18:41:22 +0530

    credit updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
 /*
+HELPS TO PLAN YOUR TIMETABLE
+Copyright (C) 2024  Sarvesh Dakhore
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+/*
  *  This file contains the events and functions applied to
  *  the document body that is common to all sections or
  *  that doesn't fit into any particular section
